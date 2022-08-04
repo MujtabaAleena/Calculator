e7d0e3cd22cc6b516e604141b80ebfc58692f0e8
MujtabaAleena <alinam.naqvi@gmail.com>
2022-08-04 19:12:29 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,37 @@
+x=6
+y=3
+sum=x+y
+print('The sum is ', sum)
+
+def remainder(x,y):
+    return(x%y)
+
+print("The remainder is ",remainder(x,y))
+
+def product(x,y):
+    return(x*y)
+
+print("The product is",product(x,y))
+
+def divide(x,y):
+    return(x/y)
+
+print("The divident is",divide(x,y))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
